4bb3b5bf33f9411370ac579ef398eb309ef5a270
risko121 <55340419+risko121@users.noreply.github.com>
2023-01-17 15:17:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// console.log("hello");
+
+// var number1=100;
+// var number2=110;
+// console.log(number1+number2);
+
+// function avg(a,b){
+//     return (a+b)/2
+// }
+
+// c=avg(1,2);
+// console.log(c)
+
+// var age = 33;
+
+// if(age > 21){
+//     console.log('you are not a kid');
+// }
+// else{
+//     console.log("you are a kid");
+// }
+
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// arr.forEach(function(Element){ //alternative of for loop
+//     console.log(Element)
+// })
+
+// function clicked(){
+    
+//     console.log("jai maharashtra ")
+// }
+b  = new Date();
+date = b.toLocaleDateString(undefined,options);
+time = b.getHours() +':'+ b.getMinutes()+':' + b.getSeconds();
+console.log(b);
